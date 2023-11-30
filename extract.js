9dc0e0e4699b59c29c5fcecdc32d3fc0520d9bd2
IvanFtv <ivan.pellega@gmail.com>
2023-11-30 14:41:40 +0000

    extract

--- extract.js
+++ extract.js
@@ -1,13 +1,53 @@
 const students = [
-    'ciccio',
-    'pippo',
+    'Achraf',
+    'Alessandro',
+    'Angelo',
+    'Barbara',
+    'Basilia',
+    'Carla',
+    'Carmelo',
+    'Chiara',
+    'Daniele',
+    'Edoardo',
+    'Esperanza',
+    'Federico',
+    'Flavia',
+    'Florencia',
+    'Gabriele',
+    'Gerta',
+    'Gessica B.',
+    'Giacomo',
+    'Gioele',
+    'Giusy',
+    'Jessica C.',
+    'Linda',
+    'Livia',
+    'Lorenzo',
+    'Ludovica',
+    'Marco',
+    'Marta',
+    'Martina',
+    'Matteo',
+    'Michele',
+    'Milena',
+    'Nicole',
+    'Paola',
+    'Ritanna',
+    'Roberta',
+    'Stefania',
+    'Thomas',
+    'Tommaso',
+    'Tudor',
+    'Valentina'
 ]
 
-function extractStudents() {
-    const num = Math.floor(Math.random() * students.length);
-    const student = students[num];
-    console.log(student);
-}
 
+function extractStudents() {
+    console.log('Il fortunato sara\'..')
+    console.log('-------------------------');
+    setTimeout(() => {
+        console.log(students[Math.floor(Math.random() * students.length)]);
+    }, 1500)
+}
 
 extractStudents();
