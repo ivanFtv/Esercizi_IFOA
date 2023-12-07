ad633f3d84627fc060315ee161bfaaf4a43b1774
IvanFtv <ivan.pellega@gmail.com>
2023-12-07 13:37:27 +0000

    update

--- UNIT2/Week3/FETCH/script.js
+++ UNIT2/Week3/FETCH/script.js
@@ -1,86 +1,92 @@
-const token = '176364dfkjfks988944';
+// CALLBACKS
+// LA CALLBACK E UNA FUNZIONE CHE VIENE PASSATA COME ARGOMENTO AD UN ALTRA FUNZIONE
+function pippo(paperino, milliseconds) {
+    console.log('Inizio operazione async');
+    setTimeout(() => {
+        console.log('Operazione async completata dopo 5 secondi');
+        x = 5+5
+        paperino(x);
+    }, milliseconds);
+    console.log('Fine operazione async');
+}
 
-// function pippo(paperino, milliseconds) {
-//     console.log('Inizio operazione async');
-//     setTimeout(() => {
-//         console.log('Operazione async completata dopo 5 secondi');
-//         x = 5+5
-//         paperino(x);
-//     }, milliseconds);
-//     console.log('Fine operazione async');
-// }
+function paperino(x) {
+    console.log(x);
+}
 
-// function paperino(x) {
-//     console.log(x);
-// }
+pippo(paperino, 2000);
 
-// pippo(paperino, 2000);
-// setTimeout(paperino, 2000)
-
-
-// function operazione1(callback1) {
-//     setTimeout(() => {
-//         console.log('Operazione 1 completata dopo 2 secondi');
-//         callback1();
-//     }, 2000);
-// }
-
-// function operazione2(callback2) {
-//     setTimeout(() => {
-//         console.log('Operazione 2 completata dopo 2 secondi');
-//         callback2();
-//     }, 2000);
-// }
-
-// function operazione3(callback3) {
-//     setTimeout(() => {
-//         console.log('Operazione 3 completata dopo 2 secondi');
-//         callback3();
-//     }, 2000);
-// }
+// ESEMPIO DI CALLBACK CONOSCIUTA IL NOSTRO FEDELE setTimeout
+setTimeout(paperino, 2000)
 
 
-// function eseguiOperazioni() {
-//     operazione1(() => {
-//         operazione2(() => {
-//             operazione3(() => {
-//                 console.log('Operazioni completate');
-//             })
-//         })
-//     })
-// }
+// CALLBACK MULTIPLE
+function operazione1(callback1) {
+    setTimeout(() => {
+        console.log('Operazione 1 completata dopo 2 secondi');
+        callback1();
+    }, 2000);
+}
+
+function operazione2(callback2) {
+    setTimeout(() => {
+        console.log('Operazione 2 completata dopo 2 secondi');
+        callback2();
+    }, 2000);
+}
+
+function operazione3(callback3) {
+    setTimeout(() => {
+        console.log('Operazione 3 completata dopo 2 secondi');
+        callback3();
+    }, 2000);
+}
+
+// RICHIAMO LE CALLBACK  IN ORDINE
+// SI DEFINISCE CALLBACK HELL
+function eseguiOperazioni() {
+    operazione1(() => {
+        operazione2(() => {
+            operazione3(() => {
+                console.log('Operazioni completate');
+            })
+        })
+    })
+}
 
 
-// eseguiOperazioni();
+eseguiOperazioni();
 
 
 
 // PROMISES
-// function recuperaDati() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const dati = {
-//                 nome: 'Mario', 
-//                 cognome: 'Rossi'
-//             };
-//             const operazioneRiuscita = false;
+// LA PROMISE RESTITUISCE UNA PROMESSA DI UNA FUTURA OPERAZIONE
+// QUANDO VIENE RISOLTA LA PROMESSA, VIENE ESEGUITA LA CALLBACK O IL CODICE CHE DEFINIAMO NEL .then() ALTRIMENTI VIENE ESEGUITO IL CODICE NEL .catch() PER LA GESTIONE DEGLI ERRORI
+function recuperaDati() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dati = {
+                nome: 'Mario', 
+                cognome: 'Rossi'
+            };
+            const operazioneRiuscita = false;
 
-//             if (operazioneRiuscita) {
-//                 resolve(dati);
-//             } else {
-//                 reject(err);
-//             }
-//         }, 2000);
-//     });
-// }
+            if (operazioneRiuscita) {
+                resolve(dati);
+            } else {
+                reject(err);
+            }
+        }, 2000);
+    });
+}
 
-// recuperaDati()
-// .then((res) => {
-//     console.log(res);
-// })
-// .catch((err) => {
-//     console.log(err.type);
-// });
+recuperaDati()
+.then((res) => { // CODICE CHE ESEGUO SE RISOLTA
+    console.log(res);
+})
+.catch((err) => { // CODICE CHE ESEGUO SE RIFIUTATA
+    console.log(err.type);
+});
 
 
 // PROMISES MULTIPLA
