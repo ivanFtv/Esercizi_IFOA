3a74899e97855698e1c502d18158404b6c6ae4ce
IvanFtv <ivan.pellega@gmail.com>
2024-01-09 17:01:24 +0000

    update

--- UNIT3/React_JSX_Bootstrap/reactapp/src/components/Lista.jsx
+++ UNIT3/React_JSX_Bootstrap/reactapp/src/components/Lista.jsx
@@ -1,17 +1,36 @@
 import { Card, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import ListButtons from './ListButtons';
+import ListButtonsSuper from './ListButtonsSuper';
+import FantasyBooks from '../../json/fantasy.json';
+import HistoryBooks from '../../json/history.json';
+import HorrorBooks from '../../json/horror.json';
+import RomanceBooks from '../../json/romance.json';
+import SciFiBooks from '../../json/scifi.json';
 
 const Lista = () => {
     const [list, setList] = useState([]);
 
+    // const categories = ['fantasy', 'history', 'horror', 'romance', 'scifi']
+    const categories = ['Fantasy', 'History', 'Horror', 'Romance', 'Scifi', 'Astronomia', 'Cucina']
+
+
     return (
         <>
             <ListButtons setList={setList} />
+            <ListButtonsSuper categories={categories} setList={setList}/>
+
+            {/* <div className="text-center my-3">
+                <Button variant="outline-warning" className="mx-3" onClick={() => setList(FantasyBooks)}>Fantasy</Button>
+                <Button variant="outline-warning" className="mx-3" onClick={() => setList(HistoryBooks)}>History</Button>
+                <Button variant="outline-warning" className="mx-3" onClick={() => setList(HorrorBooks)}>Horror</Button>
+                <Button variant="outline-warning" className="mx-3" onClick={() => setList(RomanceBooks)}>Romance</Button>
+                <Button variant="outline-warning" className="mx-3" onClick={() => setList(SciFiBooks)}>SciFi</Button>
+            </div> */}
 
             <div className="d-flex justify-content-between flex-wrap">
             {list.map((book) => 
-                <Card style={{ width: '18rem' }} key={book.asin}>
+                <Card style={{ width: '18rem' }} className="my-2" key={book.asin}>
                     <Card.Img variant="top" src={book.img} />
                     <Card.Body>
                         <Card.Title>{book.title}</Card.Title>
